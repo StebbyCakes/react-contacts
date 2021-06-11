7aae9a494846168abab247df6c23721334098c71
Esteban Carter <carterea1999@gmail.com>
2021-06-11 15:14:10 -0400

    Final Product

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
+import { Component } from 'react';
 import './App.css';
+import ContactList from './ContactList';
+import ContactsForm from './ContactsForm';
 
-function App() {
+class App extends Component{
+constructor(props) {
+    super(props);
+    this.state = {
+      contacts: [],
+      counter: 2,
+    }
+    this.addContact = this.addContact.bind(this);
+}
+componentDidMount() {
+  const contacts = [
+    {
+      id: 1,
+      firstName: 'Esteban',
+      lastName: 'Carter',
+      address: '403 ErrorCity Wheretown, DK, 01010',
+      phoneNumber: '888-888-8888'
+    }
+  ];
+  this.setState({ contacts });
+}
+
+addContact(contact){
+  contact.id = this.state.counter;
+  const contacts = [ ...this.state.contacts ];
+  contacts.push(contact);
+  this.setState((state) => ({ contacts, counter: state.counter + 1 }));
+}
+render() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Contacts</h1>
+      <ContactsForm addContact={this.addContact} />
+      <ContactList contacts={this.state.contacts} />
     </div>
   );
+}
 }
 
 export default App;
